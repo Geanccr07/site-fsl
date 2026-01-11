ed3648f6245a2c411fd04776f43f03c530525739
Geanccr07 <132221813+Geanccr07@users.noreply.github.com>
2026-01-11 17:15:52 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function onlyNumbers(value) {
   return value.replace(/\D/g, "");
 }
 
-/* Força só números enquanto digita */
+/* Força apenas números no WhatsApp */
 whatsappInput.addEventListener("input", () => {
   whatsappInput.value = onlyNumbers(whatsappInput.value);
 });
@@ -25,7 +25,7 @@ form.addEventListener("submit", async (e) => {
   const whatsapp = onlyNumbers(whatsappInput.value);
   const area = document.getElementById("area").value;
 
-  /* Validação WhatsApp (DDD + celular) */
+  /* Validação WhatsApp BR */
   if (whatsapp.length !== 11) {
     alert("Digite um WhatsApp válido com DDD (11 números).");
     whatsappInput.focus();
@@ -36,24 +36,24 @@ form.addEventListener("submit", async (e) => {
   submitBtn.innerText = "Enviando...";
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbzzdA0Wy4vT2tKaDgm9zIZwUESgdRFjdBUIycGIhEeNCVti_HB0qMFoXuz-GaH8Tspb/exec", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        nome,
-        email,
-        whatsapp,
-        area
-      })
-    });
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycby6pVcpFOqa8jkEFDPlxdQ_PeSautByDoTaZXkqTLVz5dBgD40sZObCxfMbmj2C5p4M/exec",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          nome,
+          email,
+          whatsapp,
+          area
+        })
+      }
+    );
 
     const result = await response.json();
 
-    if (result.status === "success") {
-      // 👉 REDIRECIONAMENTO APÓS CONVERSÃO
-      window.location.href = "https://chat.whatsapp.com/CCrYGei0DDrGRHfI1Jdsta";
+    if (result.success) {
+      window.location.href =
+        "https://chat.whatsapp.com/CCrYGei0DDrGRHfI1Jdsta";
     } else {
       alert("Erro ao enviar. Tente novamente.");
       submitBtn.disabled = false;
